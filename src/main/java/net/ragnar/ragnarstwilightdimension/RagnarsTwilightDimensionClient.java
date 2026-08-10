37b6e84a2fde66ca7e325025e3c01d9f34b15607
RagnarThor19 <ragnarthor102003@gmail.com>
2026-08-10 21:42:17 +0000

    Initial commit

---
+++ src/main/java/net/ragnar/ragnarstwilightdimension/RagnarsTwilightDimensionClient.java
@@ -0,0 +1,10 @@
+package net.ragnar.ragnarstwilightdimension;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RagnarsTwilightDimensionClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/ragnar/ragnarstwilightdimension/RagnarsTwilightDimensionDataGenerator.java
@@ -0,0 +1,11 @@
+package net.ragnar.ragnarstwilightdimension;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RagnarsTwilightDimensionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
